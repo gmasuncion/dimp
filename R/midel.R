b46cc6842b214d3cc8106b0e6aa60acd2c66d878
gmasuncion <gm.asuncion@hotmail.com>
2024-11-24 17:56:24 -0500

    initializing files for package

--- R/midel.R
+++ R/midel.R
@@ -6,22 +6,22 @@
 #' @return The imputed datasets with outcomes set back to NA, if they were initially missing.
 #' @author Group A
 #' @examples
-
-library(mice)
+#'<TODO>
+#' @export
 
 dimp <- function(original_data, mice_obj, y){
-  
+
   # Identify the indices of missing values in the original data for variable y
   missing_y_indices <- which(is.na(original_data[[y]]))
-  
+
   # Retrieve the imputed datasets
   imputed_datasets <- complete(mice_obj, action = 'all')  # List of imputed datasets
-  
+
   # Delete (set to NA) the values in the variable y where they were originally missing
   deleted_datasets <- lapply(imputed_datasets, function(df) {
     df[[y]][missing_y_indices] <- NA
     return(df)
   })
-  
+
   return(deleted_datasets)
 }
