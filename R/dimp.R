7fa11aa142004bc9937074553b5c36b17965559f
gmasuncion <gm.asuncion@hotmail.com>
2024-11-27 22:06:20 -0500

    documentation updated

--- R/dimp.R
+++ R/dimp.R
@@ -1,25 +1,34 @@
 #' @title Multiple Imputation and Deletion
 #' @description This function performs multiple imputation (including the outcome variable) and deletes any imputed outcome values prior to analysis.
-#' @param x The raw dataset before imputation.
-#' @param mice_obj <TODO>
+#' @param mice_obj A `mids` object created using the `mice()` function.
 #' @param y The desired outcome variable.
 #' @return The imputed datasets with outcomes set back to NA, if they were initially missing.
-#' @author Group A
+#' @author Mark Asuncion, Ting Lin, John Fei, Luke Bai, Jason Dang
 #' @examples
-#'<TODO>
+#' # Load in the mice library
+#' library(mice)
+#'
+#' # Create a mids object
+#' data <- airquality
+#' mice_obj <- mice(data, m = 5, maxit = 5, seed = 123)
+#'
+#' mice_obj_deleted <- dimp(mice_obj, "Ozone")
 #' @export
 
 dimp <- function(mice_obj, y){
+
   original_data <- complete(mice_obj, action = 0)
-  # Identify the indices of missing values in the original data for variable y
+
+  # Identify observations with missing outcomes in the original dataset
   missing_y_indices <- is.na(original_data[[y]])
 
   # Retrieve the imputed datasets in long format
   imputed_dataset <- complete(mice_obj, action = 'long', include=FALSE)
 
-  # Delete (set to NA) the values in the variable y where they were originally missing
+  # Deletion (revert imputed outcome values back to NA)
   imputed_dataset[[y]][rep(missing_y_indices, times = length(unique(imputed_dataset$.imp)))] <- NA
 
+  # Updating the datasets in the passed in mice() object
   original_data$.imp <- 0
   original_data$.id <- 1:nrow(original_data)
   imputed_data_combined <- rbind(original_data, imputed_dataset)
