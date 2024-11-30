26417c07a251d5a5143549478f1487618836439e
gmasuncion <gm.asuncion@hotmail.com>
2024-11-30 16:00:46 -0500

    tests updated, data documented

--- tests/testthat/test-dimp.R
+++ tests/testthat/test-dimp.R
@@ -11,23 +11,17 @@ test_that("dimp sets originally missing outcome values to NA in all imputations"
   # This is the original data before imputation
   original_data <- complete(mice_obj, action = 0)
 
-  # Calculate the number of missing values in the original 'Ozone' variable
-  original_missing_ozone <- sum(is.na(original_data$Ozone))
+  # Identify the observations with missing values in the original 'Ozone' variable
+  original_missing_ozone <- which(is.na(original_data$Ozone))
 
   # Extract the imputed data (in long format) from the mice.mids object returned by dimp
   imputed_data <- complete(mice_obj_deleted, action = "long", include = FALSE)
 
-  imputed_missing_ozone <- sapply(split(imputed_data$Ozone, imputed_data$.imp), function(x) sum(is.na(x)))
+  # Grabbing observations with missing outcome values after running dimp
+  imputed_missing_ozone <- sapply(split(imputed_data$Ozone, imputed_data$.imp),
+                                  function(x) which(is.na(x)))
 
-  expect_true(all(imputed_missing_ozone == original_missing_ozone),
+  expect_true(all(apply(imputed_missing_ozone, 2,
+                        function(col) all(imputed_missing_ozone == original_missing_ozone))),
               info = "Test Failed")
-})
-
-test_that("dimp returns a valid mids object", {
-
-  mice_obj <- mice(airquality, m = 5, maxit = 5, seed = 123)
-
-  mice_obj_deleted <- dimp(mice_obj, "Ozone")
-
-  expect_s3_class(mice_obj_deleted, "mids")
 })
